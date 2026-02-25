c32051d122c449a3a29bca45ff5462cb943ea8f0
beastlymeap <125094018+beastlymeap@users.noreply.github.com>
2026-02-24 20:15:06 -0600

    Create 04_measures_exploration.sql

---
+++ scripts/04_measures_exploration.sql
@@ -0,0 +1,54 @@
+/*
+===============================================================================
+Measures Exploration (Key Metrics)
+===============================================================================
+Purpose:
+    - To calculate aggregated metrics (e.g., totals, averages) for quick insights.
+    - To identify overall trends or spot anomalies.
+
+SQL Functions Used:
+    - COUNT(), SUM(), AVG()
+===============================================================================
+*/
+-- Find the Total Sales
+
+SELECT SUM(sales_amount) AS total_sales FROM gold.fact_sales;
+
+-- Find how many items are sold
+
+SELECT SUM(quantity) AS total_quantity FROM gold.fact_sales;
+
+-- Find the average selling price
+
+SELECT AVG(price) AS avg_price FROM gold.fact_sales;
+
+-- Find the Total number of Orders
+
+SELECT COUNT(order_number) AS total_orders FROM gold.fact_sales;
+SELECT COUNT(DISTINCT order_number) AS total_orders FROM gold.fact_sales;
+
+-- Find the Total number of Products
+
+SELECT COUNT(product_key) AS total_products FROM gold.dim_products;
+
+-- Find the Total number of Customers
+
+SELECT COUNT(customer_key) AS total_customers FROM gold.dim_customers;
+
+-- Find the Total number of Customers that has placed an order
+
+SELECT COUNT(DISTINCT customer_key) AS total_customers FROM gold.fact_sales;
+
+-- Generate a Report that shows all key metrics of the business
+
+SELECT 'Total Sales' as measure_name, SUM(sales_amount) AS measure_value FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Quantity' as measure_name, SUM(quantity) AS measure_value FROM gold.fact_sales
+UNION ALL
+SELECT 'Average Price' as measure_name, AVG(price) AS measure_value FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Orders' as measure_name, COUNT(DISTINCT order_number) AS measure_value FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Products' as measure_name, COUNT(product_key) AS measure_value FROM gold.dim_products
+UNION ALL
+SELECT 'Total Customers' as measure_name, COUNT(customer_key) AS measure_value FROM gold.dim_customers
